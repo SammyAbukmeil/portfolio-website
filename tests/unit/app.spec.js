e38db322ab5ac4eb0931d4f0d2a5bf6e42f5dc4e
Sammy Abukmeil <sammyabukmeil@akauk.com>
2019-06-25 17:59:58 +0100

    Get 100% test coverage

--- tests/unit/app.spec.js
+++ tests/unit/app.spec.js
@@ -3,6 +3,12 @@ import App from '@/App.vue'
 
 describe('App.vue', () => {
   const wrapper = shallowMount(App)
+  const button = wrapper.find('.dark-theme-button button') // Button to switch theme
+
+  // Set the theme back to light theme before each test
+  beforeEach(() => {
+    wrapper.setData({ lightTheme: true })
+  })
 
   it('is a Vue Instance', () => {
       expect(wrapper.isVueInstance()).toBeTruthy()
@@ -13,8 +19,13 @@ describe('App.vue', () => {
   })
 
   it('can be switched to dark theme', () => {
-    const button = wrapper.find('.dark-theme-button button')
     button.trigger('click')
     expect(wrapper.vm.lightTheme).toBeFalsy()
+  })
+
+  it('can be switched back to light theme from dark theme', () => {  
+    button.trigger('click')
+    button.trigger('click')
+    expect(wrapper.vm.lightTheme).toBeTruthy()
   })
 })
